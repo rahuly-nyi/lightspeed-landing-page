6ced15252bc745e28613d2c8fc610982ec68de14
Rahul Yadav <rahul.yadav6131766@gmail.com>
2025-12-03 16:36:05 +0530

    Added AOS
    Added AOS

---
+++ static/js/app.js
@@ -0,0 +1,12 @@
+// Initialize AOS
+AOS.init({
+  duration: 1000,
+  once: true,
+  offset: 100,
+  delay: 100,
+  easing: "ease-in-out",
+  anchorPlacement: "top-bottom",
+  anchorOn: "window",
+  anchorOnSelector: "body",
+  anchorOnSelector: "body",
+});
